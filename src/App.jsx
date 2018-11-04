cfe987a14f7cdb9518c5116d4d3622b8c9743c0b
flowck <flowck96@gmail.com>
2018-11-04 19:57:12 +0100

    🔧 Setup react-router 🔧

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const Home = () => (
+    <div>
+        <div>Home</div>
+    </div>
+);
+
+const Stats = () => (
+    <div>
+        <div>Stats</div>
+    </div>
+);
+
+const Profiles = () => (
+    <div>
+        <div>Profiles</div>
+    </div>
+);
 
 class App extends Component {
 
+    componentWillMount() {
+        fetch('https://api.github.com/search/users?q=location:Angola')
+        .then(res => res.json())
+        .then(users => console.log(users))
+        .catch(err => console.log(err));
+    }
+
     render() {
         return(
-            <div>Hello World</div>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/stats">Stats</Link>
+                    </li>
+                    <li>
+                        <Link to="/profiles">Profiles</Link>
+                    </li>
+                </ul>
+
+                <Route exact path="/" component={Home}/>
+                <Route path="/stats" component={Stats}/>
+                <Route path="/profiles" component={Profiles}/>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import react-router
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // Import App component
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>, document.getElementById('root'));
