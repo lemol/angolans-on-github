f3292c5d1b72dbfef29fbc17fe6eb360bc938b20
flowck <flowck96@gmail.com>
2018-11-05 20:50:31 +0100

    Save users in the local states.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 
 // Views / Components
 import Stats from './views/Stats';
+import Profiles from './views/Profiles';
 
 const Home = () => (
     <div>
@@ -10,18 +11,24 @@ const Home = () => (
     </div>
 );
 
-const Profiles = () => (
-    <div>
-        <div>Profiles</div>
-    </div>
-);
-
 class App extends Component {
 
+    // Constructor
+    constructor() {
+        super();
+        this.state = {
+            profiles: []
+        };
+    }
+
     componentWillMount() {
-        fetch('https://api.github.com/search/users?q=location:Angola')
+        fetch('https://api.github.com/search/users?q=location:Angola&per_page=100')
         .then(res => res.json())
-        .then(users => console.log(users))
+        .then(({ items, total_count }) => {
+            // Save the result in the current state
+            this.setState({ profiles: items, totalUsers: total_count });
+            console.log(items);
+        })
         .catch(err => console.log(err));
     }
 
@@ -39,10 +46,12 @@ class App extends Component {
                         <Link to="/profiles">Profiles</Link>
                     </li>
                 </ul>
-
-                <Route exact path="/" component={Home}/>
-                <Route path="/stats" component={Stats}/>
-                <Route path="/profiles" component={Profiles}/>
+                
+                <div className="container">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/stats" component={Stats}/>
+                    <Route path="/profiles" component={Profiles}/>
+                </div>
             </div>
         )
     }
