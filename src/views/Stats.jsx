07835e74924efeab6e6da1203020e7f231adc219
flowck <flowck96@gmail.com>
2018-11-05 19:42:50 +0100

    Add react-bootstrap

--- src/views/Stats.jsx
+++ src/views/Stats.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 // View body
-const Stats = () => (
+const Stats = (props) => (
     <div>
-        <div>Stats</div>
+        <h1>Stats</h1>
+        
+        <Row>
+            <Col sm={6} md={3}>Women</Col>
+            <Col sm={6} md={3}>Men</Col>
+            <Col sm={6} md={3}>Total</Col>
+        </Row>
     </div>
 );
 
